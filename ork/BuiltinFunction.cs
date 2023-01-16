1d5b7fe8a452f9aa0000bfd376033d765b4da3ec
jfourkiotis <john.fourkiotis@outlook.com>
2023-01-16 13:28:46 +0200

    len, first, last, rest builtin functions for arrays

--- ork/BuiltinFunction.cs
+++ ork/BuiltinFunction.cs
@@ -13,8 +13,39 @@ public static class Builtin
                 throw new OrkRuntimeException($"wrong number of arguments, got={args.Length}, want=1");
             return args[0] switch
             {
-                string s => (Int64)s.Length,
+                string s => (long)s.Length,
+                List<object?> l => (long)l.Count,
                 _ => throw new OrkRuntimeException($"argument to `len` not supported, got {TypeName(args[0])}"),
+            };
+        } },
+        { "first", args =>
+        {
+            if (args.Length != 1)
+                throw new OrkRuntimeException($"wrong number of arguments, got={args.Length}, want=1");
+            return args[0] switch
+            {
+                List<object?> l => l.Count > 0 ? l[0] : null,
+                _ => throw new OrkRuntimeException($"argument to `first` must be ARRAY, got {TypeName(args[0])}"),
+            };
+        } },
+        { "last", args =>
+        {
+            if (args.Length != 1)
+                throw new OrkRuntimeException($"wrong number of arguments, got={args.Length}, want=1");
+            return args[0] switch
+            {
+                List<object?> l => l.Count > 0 ? l[l.Count - 1] : null,
+                _ => throw new OrkRuntimeException($"argument to `last` must be ARRAY, got {TypeName(args[0])}"),
+            };
+        } },
+        { "rest", args =>
+        {
+            if (args.Length != 1)
+                throw new OrkRuntimeException($"wrong number of arguments, got={args.Length}, want=1");
+            return args[0] switch
+            {
+                List<object?> l => l.Skip(1).ToList(),
+                _ => throw new OrkRuntimeException($"argument to `last` must be ARRAY, got {TypeName(args[0])}"),
             };
         } },
     };
